63beb094946abcfd4422d31b885e77b1a658b4be
Chao <31360822+ChaoWei-git@users.noreply.github.com>
2020-02-18 15:16:25 +0800

    Delete singleNumber3.java

--- week_12/singleNumber3.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int[] singleNumber(int[] nums) {
-        int bitmask = 0,b=0;
-        for(int item:nums) bitmask ^=item;
-        int diff = bitmask &(-bitmask);
-        for(int item:nums) if((diff&item)!=0)b^=item;
-        return new int[]{b,bitmask^b};
-    }
-}
