01be7356bf439a2b931f8cd018c7aaeb809c1b9b
Chao <31360822+ChaoWei-git@users.noreply.github.com>
2020-02-18 15:16:37 +0800

    Delete countBits.java

--- week_12/countBits.java
+++
@@ -1,8 +0,0 @@
-public class Solution {
-    public int[] countBits(int num) {
-        int[] ans = new int[num + 1];
-        for (int i = 1; i <= num; ++i)
-          ans[i] = ans[i & (i - 1)] + 1;
-        return ans;
-    }
-  }
